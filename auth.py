4813ae287a2f3e4b7a1a3a3c2499d877e915df08
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2020-08-08 19:04:49 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -7,13 +7,13 @@ import os
 
 
 
-# AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
-# ALGORITHMS = os.getenv('ALGORITHMS')
-# API_AUDIENCE = os.getenv('API_AUDIENCE')
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
-AUTH0_DOMAIN = 'dev-tps54maa.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'https://capstoneaaa.herokuapp.com'
+# AUTH0_DOMAIN = 'dev-tps54maa.us.auth0.com'
+# ALGORITHMS = ['RS256']
+# API_AUDIENCE = 'https://capstoneaaa.herokuapp.com'
 
 # AuthError Exception
 
