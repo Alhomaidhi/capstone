a310966941433009217378d7c07903f86d82a564
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2020-08-08 18:55:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from auth import AuthError, requires_auth
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    setup_db(app)
     CORS(app)
 
     @app.after_request
