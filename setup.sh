3b8de1102125fbf121c7c52e2d55c88f5eb3366a
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2020-08-08 16:57:39 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-AUTH0_DOMAIN = 'dev-tps54maa.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'http://localhost:8080'
-database_path = "postgres://yweqchjuitapnn:5795859172bda08b064c6e72afc1d524999d9fcf31dd79736ac10feb807c11b8@ec2-34-200-15-192.compute-1.amazonaws.com:5432/d6l2hdnmltoj1t"
-database_path_test = "postgres://yweqchjuitapnn:5795859172bda08b064c6e72afc1d524999d9fcf31dd79736ac10feb807c11b8@ec2-34-200-15-192.compute-1.amazonaws.com:5432/d6l2hdnmltoj1t"
-
-
