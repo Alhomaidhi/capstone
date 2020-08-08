28f07fbb8d5656137d1acfab551e4b1e400e5c26
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2020-08-08 18:07:19 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,57 @@
+
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+
+database_path = os.getenv('database_path')
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.String(120))
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actor(db.Model):
+    __tablename__ = 'actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.String(120))
+    gender = db.Column(db.String(6))
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        
---
+++ setup.sh
@@ -0,0 +1,7 @@
+AUTH0_DOMAIN = 'dev-tps54maa.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'http://localhost:8080'
+database_path = "sqlite:///C://Users//Alhom//OneDrive//Desktop//Workspace//Web development//Udacity//projects//capstone//database.db"
+database_path_test = "sqlite:///C://Users//Alhom//OneDrive//Desktop//Workspace//Web development//Udacity//projects//capstone//database_test.db"
+
+
