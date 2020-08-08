ead31bad1483a3c0995de9787489623ba7d2466d
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2020-08-08 19:19:50 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
-database_path = 'postgres://yweqchjuitapnn:5795859172bda08b064c6e72afc1d524999d9fcf31dd79736ac10feb807c11b8@ec2-34-200-15-192.compute-1.amazonaws.com:5432/d6l2hdnmltoj1t'
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
