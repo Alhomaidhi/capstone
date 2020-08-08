870a4fd6b9fd3462085e9f519901fe0943ac49d9
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2020-08-08 16:33:18 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 AUTH0_DOMAIN = 'dev-tps54maa.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'http://localhost:8080'
-database_path = "sqlite:///C://Users//Alhom//OneDrive//Desktop//Workspace//Web development//Udacity//projects//capstone//database.db"
-database_path_test = "sqlite:///C://Users//Alhom//OneDrive//Desktop//Workspace//Web development//Udacity//projects//capstone//database_test.db"
+database_path = "postgres://yweqchjuitapnn:5795859172bda08b064c6e72afc1d524999d9fcf31dd79736ac10feb807c11b8@ec2-34-200-15-192.compute-1.amazonaws.com:5432/d6l2hdnmltoj1t"
+database_path_test = "postgres://yweqchjuitapnn:5795859172bda08b064c6e72afc1d524999d9fcf31dd79736ac10feb807c11b8@ec2-34-200-15-192.compute-1.amazonaws.com:5432/d6l2hdnmltoj1t"
 
 
